fef3498f4498d54d5dbae7b1fa79c50143204421
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2018-03-12 08:24:10 -0400

    remove demo code

--- demo.py
+++
@@ -1,9 +0,0 @@
-from itertools import count
-import cereal
-
-class ThingSerializer(cereal.Serializer):
-    id = cereal.Field()
-    offset = cereal.IteratorField(count())
-
-print(ThingSerializer().serialize({'id': 1}))
-print(ThingSerializer().serialize({'id': 2}))
